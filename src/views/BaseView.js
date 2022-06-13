b8691d10c9dd131eb5d29f220b7c3a208d754180
kungreat2021 <kungreat2021@outlook.com>
2022-06-13 15:24:54 +0800

    paging

--- src/views/BaseView.js
+++ src/views/BaseView.js
@@ -1,9 +1,13 @@
 import React,{useState} from 'react';
+import { Pagination } from 'antd';
 import './BaseView.css'
 import MyCarousel from "../components/MyCarousel";
 import BaseSearch from "../components/BaseSearch";
+import ListSimple from "../components/ListSimple";
 const imgs = ["https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png", "/logo192.png","https://joeschmoe.io/api/v1/random"]
-
+const pageChange = (page) =>{
+    console.log(page)
+}
 function BaseView(props){
     console.log(props.type);
     const [MyCarouselImgs] = useState(imgs);
@@ -12,6 +16,8 @@ function BaseView(props){
             <div className={"left"}>
                 <MyCarousel imgs={MyCarouselImgs}/>
                 <BaseSearch />
+                <ListSimple />
+                <Pagination defaultCurrent={1} onChange={pageChange} total={50} />
             </div>
             <div className={"right"}>
                 java-right
