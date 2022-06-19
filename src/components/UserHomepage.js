0efba021f2149f3589bd83bc9fa0af1f0c7664ce
kungreat2021 <kungreat2021@outlook.com>
2022-06-19 10:02:06 +0800

    个人主页

--- src/components/UserHomepage.js
+++ src/components/UserHomepage.js
@@ -10,19 +10,16 @@ function UserHomepage(props) {
             <Descriptions.Item label="呢称">{usercon.alias}</Descriptions.Item>
             <Descriptions.Item label="性别">纯爷们</Descriptions.Item>
             <Descriptions.Item label="状态">{usercon.state?'正常':'异常'}</Descriptions.Item>
-            <Descriptions.Item label="注册时间">{usercon.registerTime}</Descriptions.Item>
-            <Descriptions.Item label="最后上线时间" span={2}>
-                2019-04-24 18:00:00
-            </Descriptions.Item>
+            <Descriptions.Item label="注册时间" >{usercon.registerTime}</Descriptions.Item>
+            <Descriptions.Item label="邮箱" span={2}>{usercon.email}</Descriptions.Item>
             <Descriptions.Item label="Status" span={3}>
                 <Badge status="processing" text="Running"/>
             </Descriptions.Item>
             <Descriptions.Item label="总积分数">{usercon.accumulatePoints}</Descriptions.Item>
-            <Descriptions.Item label="头像">
+            <Descriptions.Item label="头像" span={2}>
                 <Image width={64} src={usercon.img}/>
             </Descriptions.Item>
-            <Descriptions.Item label="邮箱">{usercon.email}</Descriptions.Item>
-            <Descriptions.Item label="个人描述">
+            <Descriptions.Item label="个人描述" span={3}>
             <pre style={{
                 whiteSpace: "pre-wrap",
                 wordWrap: "break-word",
