9ee01ded6569c6e8a59ba79f0ae73427be2d48be
kungreat <mydreams2018@outlook.com>
2022-10-30 10:43:28 +0800

    ckedit 本地代

--- src/components/BaseCkedit.js
+++ src/components/BaseCkedit.js
@@ -1,5 +1,5 @@
 import {CKEditor} from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '../ckeditor/build/ckeditor';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import {portsUploadImg} from "../utils/HttpUtils";
 import {notification} from "antd";
 /*
